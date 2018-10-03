50b821a485a398b306d475f5ad35cc51f008fee1
Adebayo Daramola <daramola.adebayo@yahoo.com>
2018-10-03 12:44:59 +0100

    Header image changed,NavBar redesigned and whole page is now responsive

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -1,29 +1,36 @@
 $(document).ready(function() {
     var number = 0;
     $(".menu0").click(function() {
-        $(".slide" + number).slideUp();
         $(".menu" + number).removeClass('current');
         $(".menu0").addClass('current');
-        $(".slide0").slideDown();
 
         number = 0;
     });
 
     $(".menu1").click(function() {
-        $(".slide" + number).slideUp();
         $(".menu" + number).removeClass('current');
         $(".menu1").addClass('current');
-        $(".slide1").slideDown();
 
         number = 1;
     });
 
     $(".menu2").click(function() {
-        $(".slide" + number).slideUp();
         $(".menu" + number).removeClass('current');
         $(".menu2").addClass('current');
-        $(".slide2").slideDown();
 
         number = 2;
+    });
+
+    //$('.bg-img').height($(window).height());
+    // Scroll Effect
+    $('.navbar a').click(function() {
+        $('body,html').animate({
+            scrollTop:$('#' + $(this).data('value')).offset().top
+        }, 1000)
+    })
+
+    // Hamburger toggler
+    $('#js-navbar-toggle').on('click',function() {
+        $('#js-menu').toggleClass('active');
     });
 });
